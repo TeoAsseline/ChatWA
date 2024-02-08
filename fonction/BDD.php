1bbc0c05343b77530cd9070e2cd2b29f8654e374
Téo Asseline <asselineteo@gmail.com>
2024-02-08 10:50:55 +0100

    changement pour bd local

--- fonction/BDD.php
+++ fonction/BDD.php
@@ -4,9 +4,9 @@
     //--------------------------//
     function ConnexionBD(){
         ///Connexion au serveur MySQL avec PDO
-        $server = '145.14.156.192';
-        $login = 'u563109936_weidnine';
-        $mdp = 'm|8MmdqL^1E';
+        $server = 'localhost';
+        $login = 'root';
+        $mdp = '';
         $db = 'u563109936_R410_Messageri';
         try {
             $linkpdo = new PDO("mysql:host=$server;dbname=$db;charset=utf8mb4", $login, $mdp);
